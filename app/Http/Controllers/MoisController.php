9280f082621b20887a63b47018d0e7f4d1a8583a
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2024-01-09 21:59:55 +0300

    Terminer la parite Mois

--- app/Http/Controllers/MoisController.php
+++ app/Http/Controllers/MoisController.php
@@ -70,7 +70,7 @@ class MoisController extends Controller
 
                     //*********************************** */
                     return response()->json([
-                        'message' => 'L\'index '
+                        'message' => 'La valeur de l\'ancien index doit-être toujours inférieur ou égale à la valeur du nouvel index'
                     ], 403);
 
                 }else{
@@ -125,9 +125,9 @@ class MoisController extends Controller
 
         $autorisation = false;
         
-        $get_mois = Mois::find($mois_id);
+        $get_mois_update = Mois::find($mois_id);
        
-        $get_date_mois = $get_mois->date_mois;
+        $get_date_mois = $get_mois_update->date_mois;
         $get_date = Carbon::parse($get_date_mois);
         $get_year = $get_date->format("Y");
 
@@ -137,17 +137,15 @@ class MoisController extends Controller
         $date = Carbon::parse($date_mois);
         $year = $date->format('Y');
 
-        dd($year);
-
         $montant_mois = $request->montant_mois;
         $nouvel_index = $request->nouvel_index;
         $ancien_index = $request->ancien_index;
         $payer = $request->payer;
 
         // Vérifier si ce mois existe ou pas
-        if($get_mois){
+        if($get_mois_update){
 
-            if($get_mois->users_id == auth()->user()->id){
+            if($get_mois_update->users_id == auth()->user()->id){
                 
                 $validator = Validator::make($request->all(), [
                     'nom_mois' => 'alpha|required|string',
@@ -163,21 +161,60 @@ class MoisController extends Controller
                         'validator_erros' => $validator->messages()
                     ]);
                 }else{
+
+
+                    // Verification nom mois et année
+                    if($nom_mois == $get_mois_update->nom_mois){
+                        $autorisation = true;
+                    }else{
+                        // Verification si le nom mois existe dans la base de données
+                        $verification_nom_mois = DB::table('mois')->where('nom_mois', $nom_mois)->exists();
+
+                        if($verification_nom_mois){
+                            
+                            $get_mois_verifier = DB::table('mois')->where('nom_mois', $nom_mois)->first();
+
+                            $get_mois_verifier_date = Carbon::parse($get_mois_verifier->date_mois);
+                            $get_mois_verifier_year = $get_mois_verifier_date->format("Y");
+
+                            if($get_mois_verifier_year != $year){
+                                $autorisation = true;
+                            }
+                        }else{
+                            $autorisation = true;
+                        }
+                    }
+                    
+                    if($autorisation){
+
+                        if($ancien_index > $nouvel_index){
+
+                            //*********************************** */
+                            return response()->json([
+                                'message' => 'La valeur de l\'ancien index doit-être toujours inférieur ou égale à la valeur du nouvel index'
+                            ], 403);
         
-                    $mois->update([
-                        'nom_mois' => $nom_mois,
-                        'date_mois' => $date_mois,
-                        'montant_mois' => $montant_mois,
-                        'nouvel_index' => $nouvel_index,
-                        'ancien_index' => $ancien_index,
-                        'payer' => $payer,
-                        'users_id' => auth()->user()->id
-                    ]);
-        
-                    return response()->json([
-                        'mois' => $mois,
-                        'message' => 'Enregistrement effectuée!'
-                    ], 200);
+                        }else{
+                            $get_mois_update->update([
+                                'nom_mois' => $nom_mois,
+                                'date_mois' => $date_mois,
+                                'montant_mois' => $montant_mois,
+                                'nouvel_index' => $nouvel_index,
+                                'ancien_index' => $ancien_index,
+                                'payer' => $payer,
+                                'users_id' => auth()->user()->id
+                            ]);
+                
+                            return response()->json([
+                                'mois' => $get_mois_update,
+                                'message' => 'Enregistrement effectuée!'
+                            ], 200);
+                        }        
+                    }else{
+                        return response()->json([
+                            'message' => 'Ce mois existe déjà!'
+                        ],  403);
+                    }
                 }
             }else{
                 return response()->json([
